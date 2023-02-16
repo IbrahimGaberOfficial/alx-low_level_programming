9a7bb724da7046c38a6d717397cd4612ffbc0416
IbrahimGaberOfficial <ibrahim.gaber.official@gmail.com>
2023-02-16 08:22:19 -0800

    betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
+
 /**
+ * main - printing syntence
+ * Description: using main to print syntence
  * This program just print the follwoing syetence
  * Programming is like building a multilingual puzzle
+ * Return: 0
  */
+
 int main(void)
 {
 	char str_var[] = "\"Programming is like building a multilingual puzzle";
