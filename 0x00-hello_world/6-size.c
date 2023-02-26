0c4320460b7f4a23902422994fdca9c6ac7f510e
IbrahimGaberOfficial <122841058+IbrahimGaberOfficial@users.noreply.github.com>
2023-02-26 17:21:13 +0200

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,15 +8,15 @@
 
 int main(void)
 {
-        char _char = sizeof(_char);
-        int _int = sizeof(_int);
-        long int _long_int = sizeof(_long_int);
-        long long int _long_long_int = sizeof(_long_long_int);
-        float _float = sizeof(_float);
-
-        printf("Size of a char: %d byte(s)\n", _char);
-        printf("Size of an int: %d byte(s)\n", _int);
-        printf("Size of a long int: %d byte(s)\n", (int)_long_int);
-        printf("Size of a long long int: %d byte(s)\n", (int)_long_long_int);
-        printf("Size of a float: %d byte(s)\n", (int)_float);
+	char _char = sizeof(_char);
+	int _int = sizeof(_int);
+	long int _long_int = sizeof(_long_int);
+	long long int _long_long_int = sizeof(_long_long_int);
+	float _float = sizeof(_float);
+	
+	printf("Size of a char: %d byte(s)\n", _char);
+	printf("Size of an int: %d byte(s)\n", _int);
+	printf("Size of a long int: %d byte(s)\n", (int)_long_int);
+	printf("Size of a long long int: %d byte(s)\n", (int)_long_long_int);
+	printf("Size of a float: %d byte(s)\n", (int)_float);
 }
