c4d23dae06d97642b8edcbba246b66c5f2fb3afc
IbrahimGaberOfficial <122841058+IbrahimGaberOfficial@users.noreply.github.com>
2023-02-26 17:18:06 +0200

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,6 +17,6 @@ int main(void)
         printf("Size of a char: %d byte(s)\n", _char);
         printf("Size of an int: %d byte(s)\n", _int);
         printf("Size of a long int: %d byte(s)\n", (int)_long_int);
-        printf("Size of a long long: %d byte(s)\n", (int)_long_long_int);
+        printf("Size of a long long int: %d byte(s)\n", (int)_long_long_int);
         printf("Size of a float: %d byte(s)\n", (int)_float);
 }
