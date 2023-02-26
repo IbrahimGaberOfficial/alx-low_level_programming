5e01d4fcbb7c1dd72d0338a9f701149f9957eda1
IbrahimGaberOfficial <122841058+IbrahimGaberOfficial@users.noreply.github.com>
2023-02-26 17:43:55 +0200

    Create 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* main - using another function to print
+* Description: use function fputs() to print
+* Return: 1
+*/
+
+int main(void)
+{	
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
