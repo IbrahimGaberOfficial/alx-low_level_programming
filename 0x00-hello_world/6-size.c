d94fb1c5f8e1e12e1444a65dc0ec53d1cceaa879
IbrahimGaberOfficial <122841058+IbrahimGaberOfficial@users.noreply.github.com>
2023-02-26 17:15:44 +0200

    Create 6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - print size of variables based on the system
+ * Description: use sizeof() function
+ * Return: 0
+ */
+
+int main(void)
+{
+        char _char = sizeof(_char);
+        int _int = sizeof(_int);
+        long int _long_int = sizeof(_long_int);
+        long long int _long_long_int = sizeof(_long_long_int);
+        float _float = sizeof(_float);
+
+        printf("Size of a char: %d byte(s)\n", _char);
+        printf("Size of an int: %d byte(s)\n", _int);
+        printf("Size of a long int: %d byte(s)\n", (int)_long_int);
+        printf("Size of a long long: %d byte(s)\n", (int)_long_long_int);
+        printf("Size of a float: %d byte(s)\n", (int)_float);
+}
