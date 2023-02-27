89065be4168d12de3479927b7b26be6ff96e4a29
IbrahimGaberOfficial <ibrahim.gaber.official@gmail.com>
2023-02-27 11:51:51 -0800

    change value using pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - change the value of the argument using pointers
+ * @n: the pointe which hold the address of the number that will be changed
+ *
+ * Return: nothing
+ */
+
+void reset_to_98(int *n)
+{	*n = 98;
+
+}
