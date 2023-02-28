ebfa2843a13041a84d0bea3d898b74e00e0d9013
IbrahimGaberOfficial <ibrahim.gaber.official@gmail.com>
2023-02-28 02:40:17 -0800

    print array elements

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -15,34 +15,13 @@ void print_array(int *a, int n)
 
 	while (i < n)
 	{
-		int j = a[i];
-		int reminder = 0;
-		int rev_num = 0;
-		char negative = '\0';
-
-		while (j != 0)
-		{
-			reminder = j % 10;
-			rev_num = rev_num * 10 + reminder;
-			j = j / 10;
-		}
-		j = rev_num;
-		if (j < 0)
-		{
-			j = j * -1;
-			negative = '-';
-		}
 		if (i != 0)
 		{
 			_putchar(',');
 			_putchar(' ');
 		}
-		_putchar(negative);
-		while (j != 0)
-		{
-			_putchar(j % 10 + '0');
-			j = j / 10;
-		}
+		printf("%d", a[i]);
 		i++;
 	}
+
 }
