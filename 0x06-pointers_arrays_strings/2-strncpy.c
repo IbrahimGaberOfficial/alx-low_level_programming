69ff1c2dcc30625043e9a5310ac9ad8ac438519d
IbrahimGaberOfficial <ibrahim.gaber.official@gmail.com>
2023-03-01 14:23:20 -0800

    make strncpy()

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strncpy - function to do the strncpy() fnction job
+ *
+ * @dest: the string to copy into
+ * @src: the string to copy from
+ * @n: the number of cpy characters
+ *
+ * Return: the dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
