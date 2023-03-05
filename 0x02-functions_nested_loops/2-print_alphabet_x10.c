d2fcf90bc09cef67b3a6c2e2c7f9234e0bf89157
IbrahimGaberOfficial <ibrahim.gaber.official@gmail.com>
2023-03-05 04:57:52 -0800

    print alphapet 10 t imes

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - function print 10 times form 'a' to 'z'
+ *
+ * Retrun: nothing
+ */
+void print_alphabet_x10(void)
+{
+	int j = 0;
+	int i = 97;
+
+	while (j < 10)
+	{
+		while (i <= 122)
+		{
+			_putchar(i);
+			i++;
+		}
+		_putchar('\n');
+	}
+}
