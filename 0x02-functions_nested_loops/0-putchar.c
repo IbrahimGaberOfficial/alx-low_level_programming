362d0dacfa25be3e47fcabf1dffd741a06262026
IbrahimGaberOfficial <ibrahim.gaber.official@gmail.com>
2023-03-05 04:26:13 -0800

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - print "_putchar" follwed by newline
+ *
+ * Description: print "_putchar"
+ * Return: 0
+ */
+int main(void)
+{
+	printf("%s\n", "_putchar");
+	return (0);
+}
