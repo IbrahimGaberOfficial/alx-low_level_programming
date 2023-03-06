f3fe342d3fe1348cf3fcacfb7c7c0a661c50ca9b
IbrahimGaberOfficial <ibrahim.gaber.official@gmail.com>
2023-03-06 06:26:34 -0800

    strchr() clone

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strchr - search in s to find c
+ *
+ * @s: the string to search in
+ * @c: the charcter to search for
+ *
+ * Return: if c is found return the address of it, if not return NULL
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i = 1;
+
+	while (i)
+	{
+		if (s[i - 1] == c)
+		{
+			return (&s[i - 1]);
+		}
+		i++;
+	}
+}
