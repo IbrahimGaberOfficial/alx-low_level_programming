3f327e073eb37f4de1a5cf33a0872c27a40c6951
IbrahimGaberOfficial <ibrahim.gaber.official@gmail.com>
2023-03-09 02:47:38 -0800

    print string using recursion way

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _puts_recursion - print function using recursion
+ *
+ * @s: the string to be printed
+ *
+ * Return: nothing
+ */
+void _puts_recursion(char *s)
+{
+	if (s[0] == '\0')
+	{
+		_putchar('\0');
+	}
+	else
+	{
+		_putchar(s[0]);
+		_puts_recursion(s + 1);
+	}
+
+}
