ec3437667b0b1bab543b4008be66fd4f95a75e50
IbrahimGaberOfficial <ibrahim.gaber.official@gmail.com>
2023-03-09 03:04:29 -0800

    print str in rev with recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - function prints the string in revere
+ *
+ * @s: the string to be printed
+ *
+ * Return: nothing
+ */
+void _print_rev_recursion(char *s)
+{
+	if (s[0] == '\0')
+	{
+		_putchar('\0');
+	}
+	else
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(s[0]);
+	}
+}
