bab60122ddf52253ca81313a8aa4bbd66a0cada2
IbrahimGaberOfficial <ibrahim.gaber.official@gmail.com>
2023-03-12 22:52:58 -0700

    argc

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - print the number of arguments 
+ * main - print the number of arguments
  * @argc: number of command line arguments
  * @argv: array containing the program command line argumets
- * 
- * Description: program prints the number of program command line arguments
+ *
+ * * Description: program prints the number of program command line arguments
  * Return: nothing
  */
 int main(int argc, char *argv[])
