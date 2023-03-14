7e7a51da58788f427836092d1c63363af1b672d0
IbrahimGaberOfficial <ibrahim.gaber.official@gmail.com>
2023-03-14 07:10:32 -0700

    created non fixed array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * create_array - function to create array with changable size
+ *
+ * @size: the size of array
+ * @c: the char value to inistialize the array with it
+ *
+ * Return: pointer to the first address of array
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i = 0;
+	char *ptr;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	ptr = (char *)malloc(size * sizeof(char));
+
+	while (i < size)
+	{
+		*(ptr + i) = c;
+		i++;
+	}
+	return (ptr);
+}
