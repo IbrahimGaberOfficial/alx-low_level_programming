365cd43344ef1995db3323278d70aeae010e19d9
IbrahimGaberOfficial <ibrahim.gaber.official@gmail.com>
2023-03-14 07:46:38 -0700

    strdup() custom function

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _strdup - strdub() custom function
+ * function create space and dublicate str to it then
+ * return pointer to new string
+ *
+ * @str: the string to copy to new allocated memory which means the new string
+ *
+ * Return: pointer to the new string
+ */
+
+char *_strdup(char *str)
+{
+	int i = 0, j = 0;
+	char *ptr;
+
+	if (str[i] == '\0')
+	{
+		return (NULL);
+	}
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+
+	ptr = (char *)malloc(i * sizeof(char));
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+
+	while (j < i)
+	{
+		ptr[j] = str[j];
+		j++;
+	}
+
+	ptr[j] = '\0';
+
+	return (ptr);
+
+}
