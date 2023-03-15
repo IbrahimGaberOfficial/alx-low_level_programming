d5671ad3a59067b3f965e2bd33f768bdfb2acac7
IbrahimGaberOfficial <ibrahim.gaber.official@gmail.com>
2023-03-15 00:06:21 -0700

    concatenate strigs

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * str_concat - function concatenate 2 strings in new allocated memory
+ *
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: pointer to new string
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int i = 0, j = 0, Counter = 0;
+	char *new_str;
+
+	while (s1[i] != '\0')
+	{
+		i++;
+	}
+	while (s2[j] != '\0')
+	{
+		j++;
+	}
+	new_str = malloc((i + j) * sizeof(char));
+	if (new_str == NULL)
+	{
+		return (NULL);
+	}
+	/** start in concatenate strings in new string */
+	for (Counter = 0; Counter < i; Counter++)
+	{
+		new_str[Counter] = s1[Counter];
+	}
+	for (Counter = 0; Counter < j; Counter++)
+	{
+		new_str[Counter + i] = s2[Counter];
+	}
+
+	return (new_str);
+}
