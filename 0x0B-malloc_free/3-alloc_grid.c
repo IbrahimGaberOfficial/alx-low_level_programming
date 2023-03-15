d42801d86ad23eaee5bd6d801bba32732a9152f3
IbrahimGaberOfficial <ibrahim.gaber.official@gmail.com>
2023-03-14 23:20:00 -0700

    create changabel size 2d array

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.>
+#include "main.h"
+
+/**
+ * alloc_grid - function use dynamic allocation to create changable size
+ *of 2d array
+ *
+ * @width: width of array
+ * @height: height of array
+ *
+ * Return: pointer point to 2d array
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int i = 0, j = 0;
+	int **arr;
+
+	if (width < 1 || height < 1)
+	{
+		return (NULL);
+	}
+	arr = malloc(height * sizeof(int *));
+
+	if (arr == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < width; i++)
+	{
+		arr[i] = malloc(height * sizeof(int));
+		if (arr[i] == NULL)
+		{
+			for (i = i; i >= 0; i--)
+			{
+				free(arr[i]);
+			}
+			free(arr);
+			return (NULL);
+		}
+	}
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			arr[i][j] = 0;
+		}
+	}
+
+	return (arr);
+}
