bd16422de1572373b759ae81b647732ce335b585
IbrahimGaberOfficial <ibrahim.gaber.official@gmail.com>
2023-03-16 04:34:59 -0700

    check allocaed status .1

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,17 +1,17 @@
 #include "main.h"
 #include <stdlib.h>
 #include <limits.h>
-"
+
 /**
- * malloc_checked - function to check if dynamic memory allocation failed 
- * or successed, if failed, function cause normal process termination
- * with status value of 98
- *
- * @b: number of wated to allocate memory in bytes
- *
- * Return: nothing
- */
- 
+*malloc_checked - function to check if dynamic memory allocation failed
+*or successed, if failed, function cause normal process termination
+*with status value of 98
+*
+*@b: number of wated to allocate memory in bytes
+*
+*Return: nothing
+*/
+
 void *malloc_checked(unsigned int b)
 {
 	unsigned int *ptr;
@@ -19,6 +19,6 @@ void *malloc_checked(unsigned int b)
 	ptr = (unsigned int *)malloc(b);
 	if (ptr == NULL)
 		exit(98);
-	
+
 	return (ptr);
 }
