47f65c9e0e54aa796957654bca77e0f06dfe47e3
IbrahimGaberOfficial <ibrahim.gaber.official@gmail.com>
2023-03-16 15:04:17 -0700

    array - range

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * array_range - function create array with max - min elements and assign
+ * every element with value of min++ until min == max
+ *
+ * @min: first value in the array
+ * @max: last value in the array
+ *
+ * Return: pointer to first element in the array
+ */
+
+int *array_range(int min, int max)
+{
+	int i = 0, j = 0, *ptr;
+
+	if (min > max)
+		return (NULL);
+
+	ptr = malloc((max - min) * sizeof(int));
+
+	if (ptr == NULL)
+		return (NULL);
+
+	j = max - min;
+	for (i = 0; i <= j; j++)
+	{
+		ptr[i] = min++;
+	}
+
+	return (NULL);
+}
