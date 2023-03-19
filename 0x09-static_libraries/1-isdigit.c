4372a54ed3ab9c09c3672658ed9707e934f36194
IbrahimGaberOfficial <ibrahim.gaber.official@gmail.com>
2023-03-19 01:21:41 -0700

    make changes in files for betty and other

---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+/**
+ * _isdigit - function check if c is digit or not
+ *
+ * @c: argument to be checked
+ *
+ * Return: 0 if numeric charachter, otherwise if not
+ */
+
+int _isdigit(int c)
+{
+	return (isdigit(c));
+}
+
---
+++ 0x09-static_libraries/100-atoi.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+/**
+ * _atoi - function convert number in charcter form into number in intger form
+ *
+ * @s: pointer to the string
+ *
+ * Return: intger
+ */
+
+int _atoi(char *s)
+{
+	return (atoi(s));
+}
+
