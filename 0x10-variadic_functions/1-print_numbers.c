1bbfe60868b5533dc8fce4aa62cf658831f60afa
IbrahimGaberOfficial <ibrahim.gaber.official@gmail.com>
2023-03-23 16:15:34 -0700

    print arguments sprated by seprator

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,29 @@
+
+/**
+ * print_numbers - function print arguments passed to function sprated
+ * seprated by charcter "separator"
+ *
+ * @separator: the seprator char between numbers
+ * @n: number of arguments(numbers)
+ *
+ * Return: nothing
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i = 0;
+	va_list args;
+
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+	{
+		if (i != 0)
+		{
+			if (separator != NULL)
+				printf("%c", *separator);
+		}
+		printf("%d", va_arg(args, int));
+	}
+	printf("\n");
+	va_end(args);
+}
