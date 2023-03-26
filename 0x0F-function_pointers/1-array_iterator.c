c7e5862288f32eca3ea79d44ceb27e6a2130ff79
IbrahimGaberOfficial <ibrahim.gaber.official@gmail.com>
2023-03-25 17:20:14 -0700

    function pointer .2

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -17,7 +17,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 
 	if (array && size && action)
 	{
-		for (i = 0; i < size; i++)
+		for (i = 0; i < (int)size; i++)
 		{
 			action(array[i]);
 		}
