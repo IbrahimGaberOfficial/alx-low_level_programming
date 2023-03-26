0072580b1e5967f3d2b4cb178ae45110d63f1be3
IbrahimGaberOfficial <ibrahim.gaber.official@gmail.com>
2023-03-25 17:01:06 -0700

    function pointer .2

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - function takes string and function as arguments and based on
+ * function argumest in give the function the string as argumest too
+ *
+ * @name: string
+ * @f: function pointer
+ *
+ * Return: nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
