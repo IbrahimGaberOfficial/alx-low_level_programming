25d83a6d7f67528ba6e9bfa56a0330254f4ef373
IbrahimGaberOfficial <ibrahim.gaber.official@gmail.com>
2023-03-31 18:56:35 -0700

    function before main()

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -2,13 +2,12 @@
 
 /**
  * myStartFun - function that executed before the main function
- * 
+ *
  * Return: nothing
  */
 
-void myStartFun() __attribute__((constructor));
-
-void myStartFun()
+void myStartFun(void)
 {
-    printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n ");
+	printf("You're beat! and yet, you must allow,");
+	printf("\nI bore my house upon my back!\n ");
 }
