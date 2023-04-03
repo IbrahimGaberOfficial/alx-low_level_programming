b6d7d678ed9caa79dd3bc22fe61cb2a65bc4bbd3
IbrahimGaberOfficial <ibrahim.gaber.official@gmail.com>
2023-04-03 04:54:51 -0700

    struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef _DOG_H
+#define _DOG_H
+
+/**
+ * struct dog - struct for object dog
+ *
+ * @name: name
+ * @age: age
+ * @owner: owner
+ *
+ * Description: longer descripton
+ */
+
+typedef struct dog
+{
+	char *name;
+	float *age;
+	char *owner;
+} dog;
+
+#endif
